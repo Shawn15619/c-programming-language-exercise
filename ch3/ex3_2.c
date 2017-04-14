e2749068696dda9ebbcd2161a440ab312d83aa51
Shawn15619 <shawnlu0905@gmail.com>
2017-04-14 01:39:35 +0000

    update ch3

--- ch3/ex3_2.c
+++ ch3/ex3_2.c
@@ -9,11 +9,13 @@ int unescape(char s[], char t[]);
 int main(){
     char line[MAXSIZE];
     char replace[MAXSIZE * 2];
+    char newline[MAXSIZE];
     readline(line, 64);
     escape(line, replace);
     //printf("%s\n",line);
     printf("\n%s\n", replace);
-    printf("Finished!\n");
+    unescape(replace, newline);
+    printf("%s\n", newline);
     return 0;
 }
 
@@ -48,3 +50,30 @@ int escape(char s[], char t[]) {
     t[j] = '\0';
     return j;
 }
+
+int unescape(char s[], char t[]){
+    int prechar;
+    int curchar;
+    int i= 0;
+    int j = 0;
+    prechar = s[i++];
+    t[j] = prechar;
+    while((curchar = s[i++]) != '\0'){
+        if(prechar == '\\'){
+            t[++j] = curchar;
+        }else{
+            if(curchar == 't'){
+                t[j] = '\t';
+            }else if(curchar == 'n'){
+                t[j] = '\n';
+            }else{
+                t[++j] = curchar;
+            }
+        }
+        curchar = prechar;
+    }
+    t[++j] = '\0';
+}
+
+
+
