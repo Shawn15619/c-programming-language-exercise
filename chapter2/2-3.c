a05a1978455a7cfde5616315b8dbb3c255b68c63
Shawn15619 <shawnlu0905@gmail.com>
2017-07-11 11:46:48 +0800

    exercise 2-3

---
+++ chapter2/2-3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define SIZE 80
+
+char line[SIZE];
+
+int readline(char line[]);
+int htoi(char line[], int len);
+
+int main () {
+	int len = readline(line);
+	int result = htoi(line, len);
+	printf("%d\n", result);
+	return 0;
+}
+
+int readline(char line[]) {
+	int c;
+	int len = 0;
+	while ((c = getchar()) != EOF && c != '\n' && len < SIZE - 1) {
+		line[len++] = c;
+	}
+	line[len] = '\0';
+
+	return len;
+}
+
+int htoi(char line[], int len) {
+	int result = 0;
+	if (len < 3 || line[0] != '0' || (line[1] != 'x' && line[1] != 'X')) {
+		return 0;
+	}
+	for (int i = 2; i < len; i++) {
+		int temp;
+		if ('0' <= line[i] && '9' >= line[i]) {
+			temp = line[i] - '0';
+		} else if ('a' <= line[i] && 'f' >= line[i]) {
+			temp = (line[i] - 'a') + 10;
+		} else {
+			temp = (line[i] - 'A') + 10;
+		}
+		result = result * 16 + temp;
+	}
+	return result;
+}
