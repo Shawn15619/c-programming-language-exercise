084e0c59e71f9a04b4cd803f0b4d696afa359c16
Shawn15619 <shawnlu0905@gmail.com>
2017-07-14 15:59:17 +0800

    stack

---
+++ data-structure/stack/cmake-build-debug/CMakeFiles/stack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stack.dir/stack.c.o"
+  "CMakeFiles/stack.dir/list.c.o"
+  "CMakeFiles/stack.dir/main.c.o"
+  "stack.pdb"
+  "stack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
